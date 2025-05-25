d5435d0df579c6578f7ba5b75fbfb38f86c26b7b
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-05-26 00:08:22 +0100

    Create rectangle_area.py

---
+++ python_introduction/rectangle_area.py
@@ -0,0 +1,12 @@
+file_path = "alx_be_python/python_introduction/rectangle_area.py"
+
+code = """
+length = 10
+width = 5
+area = length * width
+print(f"The area of the rectangle is: {area}")
+"""
+
+with open(file_path, "w") as file:
+    file.write(code)
+  
