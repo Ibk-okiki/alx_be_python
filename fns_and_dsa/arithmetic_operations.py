ec044d9ddc91de836b5c6b7754a5e8ab76d8727e
Ibk-okiki <ibukunoluwaadelokiki@gmail.com>
2025-06-08 21:59:30 +0100

    Update arithmetic_operations.py

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -1,9 +1,10 @@
 # arithmetic_operations.py
 
-def perform_operation(num1: float, num2: float, operation: str):
-    if operation == 'add':
-        return num1 + num2
-    elif operation == 'subtract':
+def perform_operation:
+    (num1: float, num2: float, operation: str):
+        if operation == 'add':
+            return num1 + num2
+        elif operation == 'subtract':
         return num1 - num2
     elif operation == 'multiply':
         return num1 * num2
